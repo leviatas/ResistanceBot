29f446d8bf01afc6aeacd9ac3a8e9eb829bd6869
leviatas <31702513+leviatas@users.noreply.github.com>
2018-03-18 21:45:36 -0300

    Update Player.py

--- Boardgamebox/Player.py
+++ Boardgamebox/Player.py
@@ -2,9 +2,9 @@ class Player(object):
     def __init__(self, name, uid):
         self.name = name
         self.uid = uid
-        self.role = None
-        self.party = None
-        self.is_dead = False        
+        self.rol = None
+        self.afiliacion = None
+        self.esta_muerto = False        
         self.was_investigated = False
         self.is_the_insquisitor = False
         self.was_the_insquisitor = False
