3d6a03eac89b337c5a24dd1423d23136525dfb2a
leviatas <31702513+leviatas@users.noreply.github.com>
2018-03-21 12:45:53 -0300

    Update MainController.py

--- MainController.py
+++ MainController.py
@@ -145,11 +145,11 @@ def asignar_miembro(bot, update):
 		
 		# Si se suman la cantidad apropiada de miembros para la mision se vota.
 		if game.board.state.equipo_contador == game.board.state.equipo_cantidad_mision:
-			miembros_elegidos = game.get_equipo_actual(false)
+			miembros_elegidos = game.get_equipo_actual(False)
 			
 			mensaje_votacion = "Quieres elegir al siguiente equipo para la mision %d:\n" % (game.board.state.currentround + 1)
 			mensaje_votacion += miembros_elegidos
-			miembros_elegidos = game.get_equipo_actual(true)		
+			miembros_elegidos = game.get_equipo_actual(True)		
 			game.board.state.mensaje_votacion = mensaje_votacion			
 			mensaje_miembros_mision_elegidos = "El líder ha elegido a los siguientes miembros para ir a la misión:\n%s\nVoten en privado si les gusta dicho equipo." % (miembros_elegidos)			
 			bot.send_message(game.cid, mensaje_miembros_mision_elegidos, ParseMode.MARKDOWN )
