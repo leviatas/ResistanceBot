9c9c1baa1624c3eb93b15c380bc13ff97d5d7fa7
leviatas <31702513+leviatas@users.noreply.github.com>
2018-03-22 11:24:17 -0300

    Update Board.py

--- Boardgamebox/Board.py
+++ Boardgamebox/Board.py
@@ -17,7 +17,7 @@ class Board(object):
         for i in range(5):
             # Pongo la cantidad de miembros por mision como primera fila
             # pongo un espacio extra luego de 4 porque esta el * de mision en casod e mas de 6 jugadores
-            if i == 3 and self.num_players > 6
+            if i == 3 and self.num_players > 6:
                 board += " " + str(i+1) + "    "
             else:        
                 board += " " + str(i+1) + "   "
