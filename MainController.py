5e0e029a29bf495ad3efa62e2778dd1f7875a5db
leviatas <31702513+leviatas@users.noreply.github.com>
2018-03-23 10:36:07 -0300

    Update MainController.py

--- MainController.py
+++ MainController.py
@@ -470,7 +470,7 @@ def incluir_modulo(bot, update):
 		uid = callback.from_user.id
 		
 		if modulo_elegido == "Fin":
-			bot.send_message(game.cid, "Gracias por configurar el juego, para comenzar presione /startgame")
+			bot.edit_message_text("Gracias por configurar el juego, para comenzar presione /startgame" % (modulo_elegido), uid, callback.message.message_id)
 		else:
 			game.modulos.append(modulo_elegido)
 			bot.edit_message_text("Se ha incluido el modulo %s" % (modulo_elegido), uid, callback.message.message_id)
