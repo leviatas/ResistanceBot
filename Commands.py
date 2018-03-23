15a6a011da57599952c2a78be94f6044cc81c2fd
leviatas <31702513+leviatas@users.noreply.github.com>
2018-03-23 15:30:16 -0300

    Update Commands.py

--- Commands.py
+++ Commands.py
@@ -518,3 +518,10 @@ def command_toggle_debugging(bot, update):
 		# Informo que el modo de debugging ha cambiado
 		game.is_debugging = True if not game.is_debugging else False
 		bot.send_message(cid, "Debug Mode: ON" if game.is_debugging else "Debug Mode: OFF")
+		
+def command_prueba(bot, update):
+	uid = update.message.from_user.id
+	if uid == ADMIN:
+		cid = update.message.chat_id
+		game = GamesController.games.get(cid, None)		
+		MainController.final_asesino(game)
