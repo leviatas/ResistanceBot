df6cf5b673a664ffadf225f29fe7d1d32dd3eb10
leviatas <31702513+leviatas@users.noreply.github.com>
2018-03-27 16:26:13 -0300

    Update Commands.py

--- Commands.py
+++ Commands.py
@@ -314,10 +314,17 @@ def command_calltovote(bot, update):
 				if elapsed > datetime.timedelta(minutes=1):
 					# Only remember to vote to players that are still in the game
 					history_text = ""
-					for player in game.player_sequence:
-						# If the player is not in last_votes send him reminder
-						if player.uid not in game.board.state.last_votes:
-							history_text += "Es hora de votar [%s](tg://user?id=%d)!\n" % (game.playerlist[player.uid].name, player.uid)
+					if game.board.state.fase_actual is None or game.board.state.fase_actual is "votacion_del_equipo_de_mision":
+						for player in game.player_sequence:
+							# If the player is not in last_votes send him reminder
+							if player.uid not in game.board.state.last_votes:
+								history_text += "Es hora de votar [%s](tg://user?id=%d)!\n" % (game.playerlist[player.uid].name, player.uid)
+					else:
+						for player in game.board.state.equipo:
+							# If the player is not in last_votes send him reminder
+							if player.uid not in game.board.state.votos_mision:
+								history_text += "Es hora de votar [%s](tg://user?id=%d)!\n" % (game.playerlist[player.uid].name, player.uid)
+						
 					bot.send_message(cid, text=history_text, parse_mode=ParseMode.MARKDOWN)
 				else:
 					bot.send_message(cid, "Cinco minutos deben pasar para pedir que se vote!") 
