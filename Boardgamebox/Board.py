206bc1d72d2e75b61d86b074fed9d38f2b6567c7
leviatas <31702513+leviatas@users.noreply.github.com>
2018-04-05 12:07:54 -0300

    Update Board.py

--- Boardgamebox/Board.py
+++ Boardgamebox/Board.py
@@ -51,7 +51,7 @@ class Board(object):
         board += "\n--- Orden de turno  ---\n"
         for index, player in enumerate(player_sequence):
             if self.state.player_counter == index:
-                board += "*" + player.name + "*" + " " + u"\u27A1\uFE0F" + " "
+                board += "" + player.name + "" + " " + u"\u27A1\uFE0F" + " "
             else:
                 board += player.name + " " + u"\u27A1\uFE0F" + " "
         board = board[:-3]
