3035b5d7e6a191d7b7604d058cb9d9ac592c492b
leviatas <31702513+leviatas@users.noreply.github.com>
2018-04-25 12:35:30 -0300

    Update Commands.py

--- Commands.py
+++ Commands.py
@@ -541,7 +541,7 @@ def command_prueba(bot, update, args):
 		cid = update.message.chat_id
 		sdate = datetime.now().strftime("%Y-%m-%d")
 		log.info("Paso la conversion " + sdate)
-		surl = "https://www.dropbox.com/s/sy4473ohowipxke/BSGP%20Esperando%20la%20Carroza%20-%20CURRENT.jpg?raw=1&cache=%d" + sdate
+		surl = "https://www.dropbox.com/s/sy4473ohowipxke/BSGP%20Esperando%20la%20Carroza%20-%20CURRENT.jpg?raw=1&cache=" + sdate
 		log.info("URL: " + surl)
 		bot.send_photo(cid, photo=surl)
 		#bot.send_photo(cid, photo='https://www.dropbox.com/s/sy4473ohowipxke/BSGP%20Esperando%20la%20Carroza%20-%20CURRENT.jpg?raw=1&cache=%d' % (datetime.now()))
