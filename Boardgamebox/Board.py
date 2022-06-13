6cbbd2e47a79fc6d79bb2c17907ed1f158839784
Peluffo Eduardo <ePeluffo@cas.austral.edu.ar>
2022-06-13 13:37:03 -0300

    SE agregan espacios para el tablero para que quede bien alineado.

--- Boardgamebox/Board.py
+++ Boardgamebox/Board.py
@@ -25,9 +25,9 @@ class Board(object):
             # Pongo la cantidad de miembros por mision como primera fila
             # pongo un espacio extra luego de 4 porque esta el * de mision en casod e mas de 6 jugadores
             if i == 3 and self.num_players > 6:
-                board += " " + str(i+1) + "    "
+                board += " " + str(i+1) + "      "
             else:        
-                board += " " + str(i+1) + "   "            
+                board += " " + str(i+1) + "     "            
             
         board += "\n"
         
