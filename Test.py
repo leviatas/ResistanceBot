e7466486ff5968a9c16855af2fe218806d6a22b6
Peluffo Eduardo <ePeluffo@cas.austral.edu.ar>
2022-07-09 10:34:38 -0300

    SE sube archivo de test

---
+++ Test.py
@@ -0,0 +1,21 @@
+from Boardgamebox.Game import Game
+from Boardgamebox.Player import Player
+from Boardgamebox.Board import Board
+
+juego = Game(12, 13)
+
+player = Player("Maximiliano", 1)
+juego.add_player(1, player)
+player = Player("Maximiliano", 2)
+juego.add_player(2, player)
+
+player_number = len(juego.playerlist)
+# juego.board = Board(player_number, juego)
+
+print(any([True for k,v in juego.playerlist.items() if v.name == 'Maximiliano']))
+
+board = ""
+for uid in juego.playerlist:
+    board += juego.playerlist[uid].name + " " + u"\u27A1\uFE0F" + " "
+
+print(board)
